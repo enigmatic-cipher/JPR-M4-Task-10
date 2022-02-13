94b1aa2d9c7b3ea8382adf1395675da63a3f282f
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-02-13 17:02:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Given two strings containing numbers as input, print the string containing the larger number. Both the numbers in the string are non-negative.
+Input-> "1234","567"
+Output-> 1234
+"""
+
+st_1 = "1234"
+st_2 = "567"
+if int(st_1)>int(st_2):
+  print(st_1)
+
+else:
+  print(st_2)
